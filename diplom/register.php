802c0218819b4cde3fa07106000b231361ab7026
KasperV12 <90834951+KasperV12@users.noreply.github.com>
2022-08-05 12:05:32 +0600

    Add files via upload

---
+++ diplom/register.php
@@ -0,0 +1,20 @@
+<?php
+    //Запускаем сессию
+    session_start();
+
+    //Добавляем файл подключения к БД
+    require_once("dbconnect.php");
+
+    //Объявляем ячейку для добавления ошибок, которые могут возникнуть при обработке формы.
+    $_SESSION["error_messages"] = '';
+
+    //Объявляем ячейку для добавления успешных сообщений
+    $_SESSION["success_messages"] = '';
+
+    $fist_name = filter_var(trim($_POST['fist_name']), FILTER_SANITIZE_STRING);
+    $last_name = filter_var(trim($_POST['last_name']), FILTER_SANITIZE_STRING);
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_STRING);
+
+            $result_query_insert = $mysqli->query("INSERT INTO `users` (first_name, last_name, email) VALUES ('".$first_name."', '".$last_name."', '".$email."')");
+            $mysqli->close();
+?>
